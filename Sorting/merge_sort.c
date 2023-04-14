7d140a548e764722cd9f1839993983addf497da0
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-04-15 04:09:44 +0530

    added merge_sort.c

--- Sorting/merge_sort.c
+++ Sorting/merge_sort.c
@@ -1,17 +1,72 @@
-#include<stdio.h>
-int sum;
-int mergesort(int arr[],int start,int end){
-    if(start<end){
-        int mid=(start+end)/2;
-        mergesort(arr,start,mid);
-        mergesort(arr,mid+1,end);
-        sum=sum+mid;        
+#include<stdio.h>        
+
+void merge(int arr[],int start,int mid,int end){
+    int ele1=mid-start+1;
+    int ele2=end-mid;
+    int arr1[ele1],arr2[ele2];
+
+    for(int i=0;i<ele1;i++){            //adding arrays elements into divided sub arrays
+        arr1[i]=arr[start+i];
     }
-    return sum;
+    for(int j=0;j<ele2;j++){
+        arr2[j]=arr[mid+1+j];
+    }
+
+    int itr1=0,itr2=0,itr3=start;                 //sorting & merging 2 arrays
+    while(itr1<ele1 && itr2<ele2){
+
+        if(arr1[itr1]<arr2[itr2]){
+            arr[itr3]=arr1[itr1];
+            itr1++;
+        }else{
+            arr[itr3]=arr2[itr2];
+            itr2++;
+        }
+        itr3++;
+
+    }
+
+    while(itr1<ele1){
+        arr[itr3]=arr1[itr1];
+        itr1++;
+        itr3++;
+    }
+
+    while(itr2<ele2){
+        arr[itr3]=arr2[itr2];
+        itr2++;
+        itr3++;
+    }
 }
-void main(){
-    int arr[]={7,1,4,-5,-3,10,8,9,3,2};
-    int size=sizeof(arr)/sizeof(arr[0]);
-    mergesort(arr,0,9);
-    printf("%d",sum);
+
+void merge_sort(int arr[],int start,int end){
+    int mid;
+    if(start<end){
+        mid=(start+end)/2;
+        merge_sort(arr,start,mid);
+        merge_sort(arr,mid+1,end);      //dividing whole array until start<end
+        merge(arr,start,mid,end);
+    }
+}
+
+int main(){
+    
+    int size;
+    printf("Enter size:");
+    scanf("%d",&size);
+    
+    int arr[size];
+    printf("\nEnter elements:");
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    merge_sort(arr,0,size-1);      //merging 2 arrays + comparing both array elements so that elements are stored in sorted manner
+    
+    printf("\n");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
 }
