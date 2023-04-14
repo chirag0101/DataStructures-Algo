fc449ba212fad973e38aaf7df93d855160c8b4ec
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-04-14 18:05:00 +0530

    edited queue.c

--- queue.c
+++ queue.c
@@ -1,38 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
 int max=5,front=-1,rear=-1,*queue;
-void nqueue(int x){
+void enqueue(int x){
     if(rear==max-1){
-        printf("Full \n");
+        printf("QUEUE FULL!\n\n");
     }else{
         queue[rear]=x;
-        printf("Inserted \n");
+        printf("Element Inserted!\n\n");
         rear++;
     }
 }
 void show(){
-    for(int i=front;i<rear;i++){
-        printf("%d \n",queue[i]);
-        
+    if(front==rear){
+        printf("QUEUE EMPTY!\n\n");
+    }else{
+        for(int i=front;i<rear;i++){
+            printf("|%d|",queue[i]);
+        }
+        printf("\n\n");
     }
 }
 void dequeue(){
     if(front==-1 && rear==-1){
-        printf("Full \n");
+        printf("QUEUE EMPTY\n\n");
     }else{
         front++;
-        printf("Deleted \n");
+        printf("Element Deleted\n\n");
     }
 }
 
 void main(){
     queue=(int *)malloc(sizeof(int));
-    nqueue(1);
-    nqueue(2);
-    nqueue(3);
-    show();
-    dequeue();
-    show();    
-    nqueue(3);
-    show();
+    while(1){
+        int ip;
+        printf("1.ENQUEUE\n");
+        printf("2.DEQUEUE\n");
+        printf("3.DISPLAY QUEUE\n");
+        printf("4.QUIT\n");
+        scanf("%d",&ip);
+        switch (ip){
+        case 1:
+            {   int x;
+                printf("Enter element to enqueue:\n");
+                scanf("%d",&x);
+                enqueue(x);
+                break;
+            }
+        case 2:
+            {  
+                dequeue();
+                break;
+            }
+        case 3:
+                show();
+                break;
+        case 4:
+                exit(0);
+                break;
+        default:
+            printf("Invalid Input!\n\n");
+            break;
+        }
+    }
 }
