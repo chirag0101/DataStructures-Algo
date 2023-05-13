ae647e50dbf868d3f0c79fe0911a83e648635d7a
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-05-13 16:05:27 +0530

    recursion

--- recursion/power_fun.c
+++ recursion/power_fun.c
@@ -1,11 +1,25 @@
 #include<stdio.h>
+//recursive:
 int power_fun(int a,int n){
-    int p=a;
-    for(int i=0;i<n-1;i++){
-        p=p*a;                 // =2*2,4*2,8*2,16*2
+    if(n==0){
+        return 1;
     }
-    return p;
+    if(n==1){
+        return a;
+    }
+    return a*power_fun(a,n-1);
 }
+
+
+// int power_fun(int a,int n){
+//     int p=a;
+//     for(int i=0;i<n-1;i++){
+//         p=p*a;                 // =2*2,4*2,8*2,16*2
+//     }
+//     return p;
+// }
+
+
 void main(){
     int a,n;
     printf("Enter Number:");
